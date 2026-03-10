e8c3b51b7bf6f085192d63905316d5bb0f3a798b
Finlo Banarse <finlobanarse@Finlos-MacBook-Pro.local>
2026-03-10 10:03:06 +0000

    test

--- train.py
+++ train.py
@@ -29,7 +29,7 @@ from torch.distributed import init_process_group, destroy_process_group
 
 from model import GPTConfig, GPT
 from lib.utils import log_attention_svd 
-
+print("test")
 # -----------------------------------------------------------------------------
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
